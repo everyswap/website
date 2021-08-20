df7fc1d751ea1db4f811d5b1209c331abef434a2
maexx393 <manx393@gmail.com>
2021-08-20 16:50:22 -0700

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -197,13 +197,13 @@ const Blog = props => {
   return (
     <Layout path={props.location.pathname}>
       <BG />
-      <SEO title="Uniswap Blog" path={props.location.pathname} />
+      <SEO title="Everywap Blog" path={props.location.pathname} />
 
       <StyledBlog>
         <PageTitleWrapper>
           <h2 style={{ fontSize: '56px' }}>Uniswap Blog</h2>
           <p>
-            News, stories, and announcements from Uniswap.{' '}
+            News, stories, and announcements from Everyswap.{' '}
             <a style={{ paddingBottom: '1rem' }} href="/rss.xml" target="_blank">
               Subscribe
             </a>
