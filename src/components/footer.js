16cd756c41fd68497a7fcf5e23cdbdca839700c2
maexx393 <manx393@gmail.com>
2021-08-20 17:08:57 -0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -141,7 +141,7 @@ const Footer = () => {
       <StyledSection>
         <StyledFooterSection>
           <EmailSection />
-          <p>© 2020 Uniswap</p>
+          <p>© 2021 Everyswap</p>
         </StyledFooterSection>
       </StyledSection>
     </StyledFooter>
@@ -225,7 +225,7 @@ const EmailSection = () => {
       <p>Subscribe to our newsletter for updates</p>
 
       <EmailRow
-        action="https://uniswap.us19.list-manage.com/subscribe/post?u=c93471c1443f1e6365b5ca093&amp;id=7d591bff13"
+        action="https://everyswap.us19.list-manage.com/subscribe/post?u=c93471c1443f1e6365b5ca093&amp;id=7d591bff13"
         method="post"
         id="mc-embedded-subscribe-form"
         name="mc-embedded-subscribe-form"
